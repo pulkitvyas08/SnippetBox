34ca8ab9c8b82eb4d28573fad8a7c55dc661cfdd
pulkitvyas08 <e19cse284@bennett.edu.in>
2024-07-24 23:00:34 +0530

    rewrite TestPingE2E

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -37,29 +37,13 @@ func TestPing(t *testing.T) {
 }
 
 func TestPingE2E(t *testing.T) {
-	app := &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
+	app := newTestApplication(t)
 
-	// Test server
-	ts := httptest.NewTLSServer(app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
